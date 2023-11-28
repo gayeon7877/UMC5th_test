c6185607916610d18bc0239e017961f8eebe3cf7
gayeon7877 <gayean01@gmail.com>
2023-11-28 17:37:18 +0900

    Feat: Login API 구현

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import  tempRouter  from './src/routes/temp.route.js';
+import dotenv from "dotenv";
+import { userRouter } from './src/routes/user.route.js';
+import cors from "cors";
+import { specs } from './config/swagger/swagger.config.js';
+import SwaggerUi from 'swagger-ui-express';
 
-const app = express();
-const port = 3000;
+dotenv.config();
+
+const app = express();  // express 앱 초기화
+
+app.set('port', process.env.PORT || 3000);   // 서버 포트 지정
+app.use(cors());                            // cors 방식 허용
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 const myLogger = (req, res, next) => {
   console.log("LOGGED");
@@ -21,9 +33,17 @@ app.get('/hello', (req, res) => {
   res.send('Hello world!');
 });
 
+// router setting
+app.use('/temp', tempRouter);
+app.use('/user', userRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// swagger
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
+
+// error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(err.stack);
 });
 
 app.use((err, req, res, next) => {
@@ -31,14 +51,10 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.data.status).send(response(err.data));
+  console.log("error", err);
+  res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
 
-app.use('/temp', tempRouter);
-
-// error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send(err.stack);
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening on port ${app.get('port')}`);
 });
-
--- src/controllers/temp.controller.js
+++ src/controllers/temp.controller.js
@@ -1,7 +1,7 @@
 import { status } from '../../config/reponse.status.js';
 import {CheckFlag, getTempData} from '../services/temp.service.js';
 import { response } from '../../config/reponse.js';
-
+import { joinUser } from "../services/user.service.js";
 export const tempTest = (req, res, next) => {
     res.send(response(status.SUCCESS, getTempData()));
 };
