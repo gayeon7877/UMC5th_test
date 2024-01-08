2e322ed7e0b062bc8f456981fe7ce993f82b4344
gayeon7877 <gayean01@gmail.com>
2024-01-08 18:09:21 +0900

    :sparkles: [FEAT] CI/CD 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,25 @@
 
 import express from 'express';
 import { tempRouter } from '../controllers/temp.controller.js';
+import {status} from "../../config/reponse.status.js";
 
 const app = express();
 const port = 3000;
 
 // router setting
 app.use('/temp', tempRouter);
+
+app.use('/health', healthRoute);
+
+app.get('/', (req, res, next) => {
+  res.send(response(status.SUCCESS, "루트 페이지!"));
+})
+
+// error handling
+app.use((req, res, next) => {
+  const err = new BaseError(status.NOT_FOUND);
+  next(err);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
